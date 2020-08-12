217b5225a1c69f1e0b0607cc5a79e85d4d7df4a5
BlaiseD <btaylor@bPolicySolutions.com>
2020-08-12 12:44:13 -0400

    Fix for Issue 85: Missing type map configuration or unsupported mapping

--- src/AutoMapper.Extensions.ExpressionMapping/XpressionMapperVisitor.cs
+++ src/AutoMapper.Extensions.ExpressionMapping/XpressionMapperVisitor.cs
@@ -305,7 +305,8 @@ namespace AutoMapper.Extensions.ExpressionMapping
 
         protected override Expression VisitConstant(ConstantExpression node)
         {
-            if (this.TypeMappings.TryGetValue(node.Type, out Type newType))
+            if (this.TypeMappings.TryGetValue(node.Type, out Type newType)
+                && ConfigurationProvider.ResolveTypeMap(node.Type, newType) != null)
                 return base.VisitConstant(Expression.Constant(Mapper.MapObject(node.Value, node.Type, newType), newType));
             //Issue 3455 (Non-Generic Mapper.Map failing for structs in v10)
             //return base.VisitConstant(Expression.Constant(Mapper.Map(node.Value, node.Type, newType), newType));
