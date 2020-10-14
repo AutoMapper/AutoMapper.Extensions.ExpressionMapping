553c9f12f730287f1c43267716205b2a00b281c5
Michael Croes <m.croes@viscon.eu>
2020-10-14 20:08:57 +0200

    Add test for UseAsDataSource on a generic type

--- tests/AutoMapper.Extensions.ExpressionMapping.UnitTests/ExpressionMappingWithUseAsDataSource.cs
+++ tests/AutoMapper.Extensions.ExpressionMapping.UnitTests/ExpressionMappingWithUseAsDataSource.cs
@@ -42,6 +42,35 @@
             result.ShouldAllBe(expOverDTO);
         }
 
+        [Fact]
+        public void Should_Map_From_Generic_Type()
+        {
+            // Arrange
+            var mapper = CreateMapper();
+
+            var models = new List<GenericModel<bool>>()
+            {
+                new GenericModel<bool> {ABoolean = true},
+                new GenericModel<bool> {ABoolean = false},
+                new GenericModel<bool> {ABoolean = true},
+                new GenericModel<bool> {ABoolean = false}
+            };
+
+            var queryable = models.AsQueryable();
+
+            Expression<Func<DTO, bool>> expOverDTO = (dto) => dto.Nested.AnotherBoolean;
+
+            // Act
+            var q = queryable.UseAsDataSource(mapper).For<DTO>().Where(expOverDTO);
+
+            var result = q.ToList();
+
+            // Assert
+            result.ShouldNotBeNull();
+            result.Count.ShouldBe(2);
+            result.ShouldAllBe(expOverDTO);
+        }
+
         private static IMapper CreateMapper()
         {
             var mapperConfig = new MapperConfiguration(cfg =>
@@ -52,7 +81,10 @@
                     .ForMember(d => d.AnotherBoolean, opt => opt.MapFrom(s => s.ABoolean));
                 cfg.CreateMap<DTO, Model>()
                     .ForMember(d => d.ABoolean, opt => opt.MapFrom(s => s.Nested.AnotherBoolean));
-
+                cfg.CreateMap<GenericModel<bool>, DTO>()
+                    .ForMember(d => d.Nested, opt => opt.MapFrom(s => s));
+                cfg.CreateMap<GenericModel<bool>, DTO.DTONested>()
+                    .ForMember(d => d.AnotherBoolean, opt => opt.MapFrom(s => s.ABoolean));
             });
 
             var mapper = mapperConfig.CreateMapper();
@@ -72,6 +104,12 @@
         private class Model
         {
             public bool ABoolean { get; set; }
+        }
+
+
+        private class GenericModel<T>
+        {
+            public T ABoolean { get; set; }
         }
     }
 }
