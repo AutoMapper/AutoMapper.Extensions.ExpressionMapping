6926771fbd60e9dfd05db5ac4f3c9e03b593c415
Blaise Taylor <btaylor@bPolicySolutions.com>
2021-09-29 09:11:23 -0400

    Fixes Issue #126.

--- src/AutoMapper.Extensions.ExpressionMapping/PrependParentNameVisitor.cs
+++ src/AutoMapper.Extensions.ExpressionMapping/PrependParentNameVisitor.cs
@@ -1,5 +1,7 @@
 ﻿using AutoMapper.Extensions.ExpressionMapping.Extensions;
+using System.Linq;
 using System.Linq.Expressions;
+using System.Runtime.CompilerServices;
 
 namespace AutoMapper.Extensions.ExpressionMapping
 {
@@ -16,6 +18,23 @@ namespace AutoMapper.Extensions.ExpressionMapping
         public string ParentFullName { get; }
         public Expression NewParameter { get; }
 
+        protected override Expression VisitTypeBinary(TypeBinaryExpression node)
+        {
+            if (!(node.Expression is ParameterExpression))
+                return base.VisitTypeBinary(node);
+
+            if (!object.ReferenceEquals(CurrentParameter, node.GetParameterExpression()))
+                return base.VisitTypeBinary(node);
+
+            return Expression.TypeIs
+            (
+                string.IsNullOrEmpty(ParentFullName)
+                    ? NewParameter
+                    : ExpressionHelpers.MemberAccesses(ParentFullName, NewParameter),
+                node.TypeOperand
+            );
+        }
+
         protected override Expression VisitMember(MemberExpression node)
         {
             if (node.NodeType == ExpressionType.Constant)
@@ -31,6 +50,79 @@ namespace AutoMapper.Extensions.ExpressionMapping
                         : $"{ParentFullName}.{node.GetPropertyFullName()}",
                 NewParameter
             );
+        }
+
+        protected override Expression VisitMethodCall(MethodCallExpression node)
+        {
+            if (!IsParentParameterExpression())
+                return base.VisitMethodCall(node);
+
+            if (!object.ReferenceEquals(CurrentParameter, node.GetParameterExpression()))
+                return base.VisitMethodCall(node);
+
+            if (node.Method.IsStatic)
+            {
+                if (!IsExtentionMethod())
+                    return base.VisitMethodCall(node);
+
+                if (node.Method.IsGenericMethod)
+                    return Expression.Call
+                    (
+                        node.Method.DeclaringType,
+                        node.Method.Name,
+                        node.Method.GetGenericArguments(),
+                        GetNewArgumentsForStaticMethod()
+                    );
+                else
+                    return Expression.Call(node.Method, GetNewArgumentsForStaticMethod());
+            }
+
+            //instance method
+            if (node.Method.IsGenericMethod)
+            {
+                return Expression.Call
+                (
+                    GetNewParent(),
+                    node.Method.Name,
+                    node.Method.GetGenericArguments(),
+                    node.Arguments.ToArray()
+                );
+            }
+            else
+            {
+                return Expression.Call
+                (
+                    GetNewParent(),
+                    node.Method,
+                    node.Arguments
+                );
+            }
+
+            Expression[] GetNewArgumentsForStaticMethod()
+            {
+                Expression[] arguments = node.Arguments.ToArray();
+                arguments[0] = GetNewParent();
+                return arguments.ToArray();
+            }
+
+            Expression GetNewParent()
+                => string.IsNullOrEmpty(ParentFullName)
+                ? NewParameter
+                : ExpressionHelpers.MemberAccesses(ParentFullName, NewParameter);
+
+            bool IsParentParameterExpression()
+            {
+                if (node.Method.IsStatic)
+                    return node.Arguments[0] is ParameterExpression;
+
+                if (!node.Method.IsStatic)
+                    return node.Object is ParameterExpression;
+
+                return false;
+            }
+
+            bool IsExtentionMethod()
+                => node.Method.IsDefined(typeof(ExtensionAttribute), true);
         }
     }
 }
