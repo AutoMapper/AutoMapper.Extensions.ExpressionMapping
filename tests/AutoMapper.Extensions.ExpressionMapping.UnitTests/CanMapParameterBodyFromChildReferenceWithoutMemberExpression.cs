3bb5736facf1575a9ff0f111fac496c509c119c0
Blaise Taylor <btaylor@bPolicySolutions.com>
2022-11-25 09:09:49 -0500

    Fixes Issue #152. EF.Property not mapped correctly.

---
+++ tests/AutoMapper.Extensions.ExpressionMapping.UnitTests/CanMapParameterBodyFromChildReferenceWithoutMemberExpression.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using Xunit;
+
+namespace AutoMapper.Extensions.ExpressionMapping.UnitTests
+{
+    public class CanMapParameterBodyFromChildReferenceWithoutMemberExpression
+    {
+        [Fact]
+        public void Can_map_parameter_body_from_child_reference_without_member_expression()
+        {
+            // Arrange
+            var config = new MapperConfiguration(c =>
+            {
+                c.CreateMap<TestCategory, TestProductDTO>()
+                   .ForMember(p => p.Brand, c => c.MapFrom(p => EF.Property<int>(p, "BrandId"))); ;
+
+                c.CreateMap<TestProduct, TestProductDTO>()
+                  .IncludeMembers(p => p.Category);
+            });
+
+            config.AssertConfigurationIsValid();
+            var mapper = config.CreateMapper();
+
+            var products = new List<TestProduct>() {
+                new TestProduct { }
+              }.AsQueryable();
+
+            //Act
+            Expression<Func<TestProductDTO, bool>> expr = x => x.Brand == 2;
+            var mappedExpression = mapper.MapExpression<Expression<Func<TestProduct, bool>>>(expr);
+
+            //Assert
+            Assert.Equal("x => (Convert(Property(x.Category, \"BrandId\"), Int32) == 2)", mappedExpression.ToString());
+        }
+
+        public class TestCategory
+        {
+            // Has FK BrandId
+        }
+        public class TestProduct
+        {
+            public TestCategory? Category { get; set; }
+        }
+
+        public class TestProductDTO
+        {
+            public int Brand { get; set; }
+        }
+    }
+}
---
+++ tests/AutoMapper.Extensions.ExpressionMapping.UnitTests/CanMapParameterBodyWithoutMemberExpression.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using Xunit;
+
+namespace AutoMapper.Extensions.ExpressionMapping.UnitTests
+{
+    public class CanMapParameterBodyWithoutMemberExpression
+    {
+        [Fact]
+        public void Can_map_parameter_body_without_member_expression()
+        {
+            // Arrange
+            var config = new MapperConfiguration(c =>
+            {
+                c.CreateMap<TestProduct, TestProductDTO>()
+                  .ForMember(p => p.Brand, c => c.MapFrom(p => EF.Property<int>(p, "BrandId")));
+            });
+
+            config.AssertConfigurationIsValid();
+            var mapper = config.CreateMapper();
+
+            var products = new List<TestProduct>() {
+                new TestProduct { }
+              }.AsQueryable();
+
+            //Act
+            Expression<Func<TestProductDTO, bool>> expr = x => x.Brand == 2;
+            var mappedExpression = mapper.MapExpression<Expression<Func<TestProduct, bool>>>(expr);
+
+            //Assert
+            Assert.Equal("x => (Convert(Property(x, \"BrandId\"), Int32) == 2)", mappedExpression.ToString());
+        }
+
+        public class TestProduct
+        {
+            // Empty, has shadow key named BrandId
+        }
+
+        public class TestProductDTO
+        {
+            public int Brand { get; set; }
+        }
+    }
+}
