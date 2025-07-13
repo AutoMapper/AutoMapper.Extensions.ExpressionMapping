36823249bd83ce63c1a2673ad23266ea5d000d95
Blaise Taylor <btaylor@bPolicySolutions.com>
2025-07-13 10:43:39 -0400

    Fixes Issue #185.

---
+++ tests/AutoMapper.Extensions.ExpressionMapping.UnitTests/SetPropertyCalls.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace AutoMapper.Extensions.ExpressionMapping.UnitTests
+{
+    public class SetPropertyCalls<TSource>
+    {
+        public SetPropertyCalls<TSource> SetProperty<TProperty>(
+        Func<TSource, TProperty> propertyExpression,
+        Func<TSource, TProperty> valueExpression)
+        => throw new InvalidOperationException();
+
+
+        public SetPropertyCalls<TSource> SetProperty<TProperty>(
+            Func<TSource, TProperty> propertyExpression,
+            TProperty valueExpression)
+            => throw new InvalidOperationException();
+    }
+}
